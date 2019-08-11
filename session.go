acac912628b2112859eddbb50fab90e1f028ae32
Xiangrui2019 <xiangrui@aiursoft.com>
2019-08-11 18:04:58 +0800

    add alls

--- session.go
+++ session.go
@@ -1,7 +1,17 @@
 package go_redis_session
 
+import "github.com/gin-gonic/gin"
+
 type Session struct{}
 
 func NewSession() *Session {
 	return &Session{}
+}
+
+func (session *Session) Set(context *gin.Context, key string, value string) error {
+
+}
+
+func (session *Session) Get(context *gin.Context, key string) (string, error) {
+
 }
